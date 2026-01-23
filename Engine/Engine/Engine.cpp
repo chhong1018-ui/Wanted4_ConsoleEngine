f025f238fe1ca1b8d318d4f7cd1bc53f9057b5f4
hch <chhong1018@gmail.com>
2026-01-23 18:00:59 +0900

    콘솔 엔진 프로젝트 시작
    - 게임 루프 추가
    - 프레임 시간 계산
    - 키보드 입력 처리 추가
    - ESC 키 종료 (테스트)

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -16,12 +16,11 @@ namespace Wanted
 	{
 		// 시계의 정밀도.
 		LARGE_INTEGER frequency;
-		QueryPerformanceCounter(&frequency);
+		QueryPerformanceFrequency(&frequency);
 
 		//프레임 계산용 변수.
-		//int64_t
-		__int64 currentTime = 0;
-		__int64 previousTime = 0;
+		int64_t currentTime = 0;
+		int64_t previousTime = 0;
 
 		// 하드웨어 타이머로 시간 구하기
 		LARGE_INTEGER time;
@@ -44,11 +43,10 @@ namespace Wanted
 			QueryPerformanceCounter(&time);
 			currentTime = time.QuadPart;
 
-
-
 			// 프레임 시간 계산.
 			float deltaTime
 				= static_cast<float>(currentTime - previousTime);
+
 			// 초단위 변환.
 			deltaTime = deltaTime
 				/ static_cast<float>(frequency.QuadPart);
@@ -56,7 +54,7 @@ namespace Wanted
 			// 고정 프레임.
 			if (deltaTime >= oneFrameTime)
 			{
-				processInput();
+				ProcessInput();
 
 				// 프레임 처리.
 				Tick(deltaTime);
@@ -66,63 +64,65 @@ namespace Wanted
 				previousTime = currentTime;
 
 				// 현재 입력 값을 이전 입력 값으로 저장.
-				
+
 				for (int ix = 0; ix < 255; ++ix)
 				{
-					keyStates[ix].wasKeyDown = keyStates[ix].isKeyDown;
+					keyStates[ix].wasKeyDown
+						= keyStates[ix].isKeyDown;
 				}
 			}
-
-			//todo: 정리 작업.
-			std::cout << "Engine has been shutdown....\n";
 		}
 
-		void Engine::QuitEngine()
-		{
-			isQuit = true;
-		}
+		//Todo: 정리 작업.
+		std::cout << "Engine has been shutdown....\n";
+	}
 
-		bool Engine::GetKeyDown(int keyCode)
-		{
-			return KeyStates[keyCode].isKeyDown
-				&& !keyStates[keyCode].wasKeyDown;
-		}
+	void Engine::QuitEngine()
+	{
+		isQuit = true;
+	}
 
-		bool Engine::GetKeyUp(int keyCode)
-		{
-			return !KeyStates[keyCode].isKeyDown
-				&& keyStates[keyCode].wasKeyDown;
-		}
+	bool Engine::GetKeyDown(int keyCode)
+	{
+		return keyStates[keyCode].isKeyDown
+			&& !keyStates[keyCode].wasKeyDown;
+	}
 
-		bool Engine::GetKey(int keyCode)
-		{
-			return KeyStates[keyCode].isKeyDown;
-		}
+	bool Engine::GetKeyUp(int keyCode)
+	{
+		return !keyStates[keyCode].isKeyDown
+			&& keyStates[keyCode].wasKeyDown;
+	}
 
-		void Engine::processInput()
-		{
-			// 키 마다 입력 읽기.
-			// !!! 운영체제가 제공하는 기능을 사용할 수 밖에 없음.
-			for (int ix = 0; ix < 255; ++ix)
-			{
-				keyStates[ix].isKeyDown
-					= GetAsyncKeyState(ix) & 0X8000 > 0 ? true : false;
-			}
-		}
+	bool Engine::GetKey(int keyCode)
+	{
+		return keyStates[keyCode].isKeyDown;
+	}
 
-		void Engine::Tick(float deltaTime)
+	void Engine::ProcessInput()
+	{
+		// 키 마다 입력 읽기.
+		// !!! 운영체제가 제공하는 기능을 사용할 수 밖에 없음.
+		for (int ix = 0; ix < 255; ++ix)
 		{
-			std::cout << "DeltaTime: " << deltaTime
-				<< ", FPS: " << (1.0f / deltaTime) << "\n";
-
-			// ESC키 눌리면 종료.
-			if (GetKeyDown(VK_ESCAPE))
-			{
-				QuitEngine();
-			}
-		}
-
-		void Engine::Draw()
-		{
+			keyStates[ix].isKeyDown
+				= GetAsyncKeyState(ix) & 0X8000 > 0 ? true : false;
 		}
 	}
+
+	void Engine::Tick(float deltaTime)
+	{
+		std::cout << "DeltaTime: " << deltaTime
+			<< ", FPS: " << (1.0f / deltaTime) << "\n";
+
+		// ESC키 눌리면 종료.
+		if (GetKeyDown(VK_ESCAPE))
+		{
+			QuitEngine();
+		}
+	}
+
+	void Engine::Draw()
+	{
+	}
+}
